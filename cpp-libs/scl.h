b11c12a532e73536cad2c7c2a8c00b16c20bb8f6
Mohammad Amin Khakzadan <mak12776@gmail.com>
2020-01-05 06:53:15 +0330

    backup

--- cpp-libs/scl.h
+++ cpp-libs/scl.h
@@ -18,12 +18,11 @@
 #include "math.h"
 #include "safe.h"
 
+#include "memory.h"
+#include "io.h"
+#include "pxld.h"
+#include "machine.h"
+
 #include "arrays.h"
 #include "vectors.h"
 #include "buffers.h"
-
-#include "io.h"
-#include "string.h"
-#include "memory.h"
-#include "pxld.h"
-#include "machine.h"
