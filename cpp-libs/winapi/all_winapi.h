a9255a324bfbe82963a26b5d7aa9538cf3d86614
Mohammad Amin Khakzadan <mak12776@gmail.com>
2020-10-23 02:57:40 +0330

    backup

---
+++ cpp-libs/winapi/all_winapi.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../scl/all.h"
+
+#include <Windows.h>
+#include <processthreadsapi.h>
+
+#include <cstdint>
+#include <cstdio>
+#include <functional>
+
+#include "types.h"
+#include "time.h"		// <Windows.h> "scl/all.h"
+#include "functions.h"	// <funtional>		
