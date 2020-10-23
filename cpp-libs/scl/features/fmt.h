914066bd62be6c11353affb8de03cdb4645901aa
Mohammad Amin Khakzadan <mak12776@gmail.com>
2020-10-24 00:03:27 +0330

    backup

--- cpp-libs/scl/features/fmt.h
+++ cpp-libs/scl/features/fmt.h
@@ -4,32 +4,102 @@ namespace scl
 {
 	namespace fmt
 	{
-		/* {0:i32} {1:i32}
-		*/
-		enum class num_t : uint8_t
+		// c standards: 127 parameters in one function definition
+		constexpr uint8_t max_argument_index = 127 - 1;
+
+		enum class fmt_num_t : uint8_t
 		{
-			SUCCESS = 0,
-			UNMATCHED_CURLY_BRACKET
+			SUCCESS = 0, 
+			UNMATCHED_CURLY_BRACKET,
+			SINGLE_LEFT_CURLY_BRACKET,
+			SINGLE_RIGHT_CURLY_BRACKET,
 		};
+
+		struct fmt_err_t
+		{
+			fmt_num_t num;
+			size_t index;
+		};
+
+		fmt_err_t default_fmt_err{ fmt_num_t::SUCCESS, 0 };
+
+		static inline void set(fmt_num_t errnum, size_t index) 
+		{
+			default_fmt_err.num = errnum; default_fmt_err.index = index;
+		}
+
+		// library functions:
+
+		// get length of fmt
+		// write to a file
+
+		// write to a buffer
+		// allocate memory and write buffer
 
 		static inline size_t get_len(const char *fmt, size_t len)
 		{
 			size_t index = 0;
 			size_t total = len;
+			constexpr size_t invalid_total = 0;
 
-			while (index < len)
+			// "finite state machine"
+
+		main_check:
+			if (index == len)
+				return total;
+
+		main_switch:
+			switch (fmt[index])
 			{
-				if (fmt[index] == '{')
-				{
-					index += 1;
-					if (index == len)
-						return total;
-
-					if (fmt[index] == '{')
-						total -= 1;
-				}
+			case '{': 
 				index += 1;
+				goto left_curly_bracket;
+
+			case '}':
+				index += 1;
+				goto right_curly_bracket;
+
+			default:
+				index += 1;
+				goto main_check;
 			}
+
+		right_curly_bracket:
+			if ((index == len) || (fmt[index] != '}'))
+			{
+				set(fmt_num_t::SINGLE_RIGHT_CURLY_BRACKET, index - 1);
+				return invalid_total;
+			}
+
+			index += 1;
+			total -= 1;
+			goto main_check;
+
+		left_curly_bracket:
+
+			if (index == len)
+			{
+				set(fmt_num_t::SINGLE_LEFT_CURLY_BRACKET, index - 1);
+				return invalid_total;
+			}
+
+			switch (fmt[index])
+			{
+			case '{':				// double_left_curly_bracket
+				index += 1;
+				total -= 1;
+				goto main_check;
+
+			case ':':
+				index += 1;
+				goto colon;
+
+			default:
+				
+			}
+
+		colon:
+
 		}
 
 		static inline size_t malloc_fmt(const char *fmt, ...)
