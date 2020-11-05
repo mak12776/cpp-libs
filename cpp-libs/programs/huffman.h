e97f367c705b9c9e065b8a964133389add67b6e2
Mohammad Amin Khakzadan <mak12776@gmail.com>
2020-11-05 19:19:21 +0330

    backup

--- cpp-libs/programs/huffman.h
+++ cpp-libs/programs/huffman.h
@@ -5,22 +5,5 @@
 namespace bith
 {
 	using namespace scl;
-
-	template <typename data_type>
-	struct data_count_t
-	{
-		data_type data;
-		size_t count;
-	};
-
-	template <typename data_type>
-	struct remaining_t
-	{
-		data_type value;
-	};
-
-	struct segmented_buffer_t
-	{
-
-	};
+	
 }
