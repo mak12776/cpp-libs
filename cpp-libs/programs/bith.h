a7958da64309ff83f0e4915ce7e6bde595a1aea2
Mohammad Amin Khakzadan <mak12776@gmail.com>
2020-11-05 21:01:42 +0330

    backup

--- cpp-libs/programs/bith.h
+++ cpp-libs/programs/bith.h
@@ -10,32 +10,51 @@ namespace bith
 	
 	struct segment_buffer_t
 	{
-		size_t size;
-		size_t len;
+		size_t data_count_size;
+		size_t data_count_len;
 		size_t *count_array;
 
 		struct
 		{
 			size_t bits;
 			size_t size;
-			ubyte *buffer;
-		} buffer;
+			struct
+			{
+				ubyte *pntr;
+				size_t size;
+			} buffer;
+		} data;
 
 		struct
 		{
 			size_t bits;
 			size_t size;
-			ubyte *buffer;
+			ubyte *pntr;
 		} remaining;
 	};
 
+	typedef bool(&size_manager_t)(size_t &);
+
+	bool double_size_manager(size_t &size)
+	{
+		if (size == 0)
+			size = 1024;
+		math::safe_mul(size, (size_t)2, size);
+		err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__);
+		return true;
+	}
+
+	template <size_manager_t size_manager = double_size_manager>
 	static inline void count_bits(size_t data_bits, ubuffer_t &buffer, segment_buffer_t &result)
 	{
-		size_t buffer_bits;
-		size_t data_size;
-
 		math::safe_mul(buffer.size, (size_t)8, buffer_bits);
-		data_size = (data_bits / 8) + (data_bits % 8) ? 1 : 0;
+
+		result.data.bits = data_bits;
+		result.data.size = (data_bits / 8) + (data_bits % 8) ? 1 : 0;
+
+		result.size = 0;
+		if (size_manager(result.size))
+			return;
 
 
 	}
