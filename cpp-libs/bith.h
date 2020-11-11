4ea466d5928f9251e8c268f803585674479adb6d
Mohammad Amin Khakzadan <mak12776@gmail.com>
2020-11-11 11:16:23 +0330

    more

--- cpp-libs/bith.h
+++ cpp-libs/bith.h
@@ -30,6 +30,8 @@ namespace bith
 
 			size_t bits;
 			size_t size;
+
+			size_t possible_data_number;
 		} info;
 
 		struct
@@ -52,31 +54,21 @@ namespace bith
 	template <typename data_type, size_manager_t size_manager>
 	static inline void count_primitive_types(ubuffer_t &buffer, segment_buffer_t &result)
 	{
-		size_t possible_data_number;
-		size_t cleaner_start_index;
-
 		data_type *pntr;
 		data_type *end;
+		size_t cleaner_start_index;
 
 		// --- calculating informations ---
 
-		// calculating possible data number
-		math::safe_pow<size_t>((size_t)2, sizeof(data_type) * 8, possible_data_number);
-		if (err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__)) return;
-
 		// calculating remaining bits & size
-		result.remaining.bits = buffer_bits % result.info.bits;
-		result.remaining.size = get_bytes_per_bits(result.remaining.bits);
+		 result.remaining.bits = result.info.buffer_bits % result.info.bits;
+		result.remaining.size = get_bytes_per_bits(result.remaining.bits); 
 
-		// choose a minimum allocation size
+		// calculating counts size & len
 		size_manager(result.counts.size);
 		if (err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__)) return;
-		result.counts.size = std::min<size_t>(possible_data_number, result.counts.size);
+		result.counts.size = std::min<size_t>(result.info.possible_data_number, result.counts.size);
 		result.counts.len = 0;
-
-		// calculating data bytes
-		math::safe_mul(result.counts.size, result.info.size, data_bytes);
-		if (err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__)) return;
 
 		// ---- memory allocations --
 
@@ -91,7 +83,10 @@ namespace bith
 		// allocate memory for data pntr
 		result.counts.data_pntr = mem::safe_malloc(data_bytes);
 		if (err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__))
-			return cleaner::finish(cleaner_start_index);
+		{
+			cleaner::finish(cleaner_start_index);
+			return;
+		}
 		cleaner::add_free(result.counts.data_pntr);
 
 		// allocate memory for counts pntr
@@ -103,21 +98,30 @@ namespace bith
 		}
 		cleaner::add_free(result.counts.counts_pntr);
 
+		// --- main process ---
+
 		pntr = buffer.pntr;
-		end = buffer.pntr + (buffer.size / sizeof(data_type));
+		end = buffer.pntr + (buffer.size / result.info.size);
 	}
 
 
 	template <size_manager_t size_manager = double_size_manager>
 	static inline void count_bits(size_t data_bits, ubuffer_t &buffer, segment_buffer_t &result)
 	{
-		// calculating data informations
+		// calculating buffer informations
 		result.info.buffer_size = buffer.size;
-		result.info.buffer_bits = buffer.size * 8;
+		math::safe_mul(buffer.size, (size_t)8, result.info.buffer_bits);
+		if (err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__)) return;
 
+		// calculating data informations
 		result.info.bits = data_bits;
 		result.info.size = get_bytes_per_bits(data_bits);
 
+		// calculating possible data number
+		math::safe_pow<size_t>((size_t)2, result.info.bits, result.info.possible_data_number);
+		if (err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__)) return;
+
+		// --- [main process] ---
 		if (result.info.bits % 8 == 0)
 		{
 			if (result.info.size == sizeof(uint8_t))
