9c51bd255466a65ff2eed867390675ba33cf6bbd
Mohammad Amin Khakzadan <mak12776@gmail.com>
2020-11-11 10:04:43 +0330

    more

--- cpp-libs/bith.h
+++ cpp-libs/bith.h
@@ -6,14 +6,14 @@ namespace bith
 {
 	using namespace scl;
 
-	typedef bool(&size_manager_t)(size_t &);
+	typedef void(&size_manager_t)(size_t &);
 
-	bool double_size_manager(size_t &size)
+	void double_size_manager(size_t &size)
 	{
 		if (size == 0)
 			size = 1024;
 		math::safe_mul(size, (size_t)2, size);
-		return err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__);
+		err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__);
 	}
 
 	struct segment_buffer_t
@@ -49,6 +49,11 @@ namespace bith
 		data_type *end = buffer.pntr + (buffer.size / sizeof(data_type));
 	}
 
+	static constexpr inline size_t get_bytes_per_bits(size_t bits)
+	{
+		return (bits / 8) + ((bits % 8) ? 1 : 0);
+	}
+
 	template <size_manager_t size_manager = double_size_manager>
 	static inline void count_bits(size_t data_bits, ubuffer_t &buffer, segment_buffer_t &result)
 	{
@@ -56,15 +61,18 @@ namespace bith
 		size_t possible_data_number;
 		size_t cleaner_start_index = cleaner::get_index();
 
+		// computing buffer bits
 		math::safe_mul(buffer.size, (size_t)8, buffer_bits);
 		if (err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__))
 			return;
 
-		// calculate bits
+		// calculating data informations
 		result.info.bits = data_bits;
-		result.info.size = (data_bits / 8) + (data_bits % 8) ? 1 : 0;
+		result.info.size = get_bytes_per_bits(data_bits);
 
 		// calculate remaining bits & size
+		result.remaining.bits = buffer_bits % data_bits;
+		result.remaining.size = get_bytes_per_bits(result.remaining.bits);
 
 		// calculate possible data number
 		math::safe_pow((size_t)2, data_bits, possible_data_number);
@@ -73,28 +81,41 @@ namespace bith
 
 		// choose a minimum allocation size
 		size_manager(result.counts.size);
-		result.counts.size = (std::min)<size_t>(possible_data_number, result.counts.size);
-
-		// calculate data bytes
-		math::safe_mul(result.info.size, result.counts.size, result.info.data_bytes);
 		if (err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__))
 			return;
+		result.counts.size = std::min<size_t>(possible_data_number, result.counts.size);
+
+		// calculating data bytes
+		math::safe_mul(result.counts.size, result.info.size, result.info.data_bytes);
+		if (err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__))
+			return;
+
+		// allocate memory for remaining
+		result.remaining.pntr = mem::safe_malloc(result.remaining.size);
+		if (err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__))
+			return;
+		cleaner::add_free(result.remaining.pntr); // from now we need call cleaner::finish at end
 
 		// allocate memory for data pntr
 		result.counts.data_pntr = mem::safe_malloc(result.info.data_bytes);
 		if (err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__))
+		{
+			cleaner::finish(cleaner_start_index);
 			return;
-		cleaner::add_free(result.counts.data_pntr);
+		}
+		cleaner::add_free(result.counts.data_pntr); 
 
 		// allocate memory for counts pntr
-		result.counts.counts_pntr = mem::safe_malloc_array<size_t>();
+		result.counts.counts_pntr = mem::safe_malloc_array<size_t>(result.info.size);
 		if (err::check_push_file_info(__FILE__, __LINE__, __FUNCTION__))
+		{
+			cleaner::finish(cleaner_start_index);
 			return;
+		}
 		cleaner::add_free(result.counts.counts_pntr);
 
-		// allocate memory for remaining
-
 		// call necessary functions and done the fucking hell job.
+
 	}
 
 	static inline void count_bits(size_t data_bits, const char *name, segment_buffer_t &result)
