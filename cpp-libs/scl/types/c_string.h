9ff20fd7849ad27e1e1c36039c1d6afc61faeba0
Mohammad Amin Khakzadan <mak12776@gmail.com>
2020-11-15 18:26:23 +0330

    more

--- cpp-libs/scl/types/c_string.h
+++ cpp-libs/scl/types/c_string.h
@@ -2,18 +2,16 @@
 
 namespace scl
 {
-
-
 	template <typename value_type>
-	struct base_const_string_t
+	struct c_base_string_t
 	{
 		const value_type *const pntr;
 		const size_t len;
 
-		base_const_string_t(const char *pntr) : pntr(pntr), len(strlen(pntr))
+		c_base_string_t(const char *pntr) : pntr(pntr), len(strlen(pntr))
 		{ }
 	};
 
-	using c_ustring_t = base_const_string_t<unsigned char>;
-	using c_string_t = base_const_string_t<char>;
+	using c_ustring_t = c_base_string_t<unsigned char>;
+	using c_string_t = c_base_string_t<char>;
 }
