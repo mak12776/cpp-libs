179dd661de772c374332934b8a2b68fafb707c94
Mohammad Amin Khakzadan <mak12776@gmail.com>
2020-11-22 17:33:28 +0330

    more

--- cpp-libs/scl/types/linked_array.h
+++ cpp-libs/scl/types/linked_array.h
@@ -3,8 +3,14 @@
 namespace scl
 {
 	template <typename data_type>
+	struct linked_array_node_t
+	{
+		data_type *pntr;
+		linked_array_node_t<data_type> *next;
+	};
+
+	template <typename data_type, size_t array_size>
 	struct linked_array_t
 	{
-
 	};
 }
