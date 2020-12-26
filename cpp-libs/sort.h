dbc4e422be0f7a4359ff59446e5a402ef4ac7b99
Mohammad Amin Khakzadan <mak12776@gmail.com>
2020-12-26 23:23:38 +0330

    more

--- cpp-libs/sort.h
+++ cpp-libs/sort.h
@@ -3,5 +3,16 @@
 
 namespace sort
 {
-
+	template <typename data_type>
+	void insertion_sort(data_type *pntr, size_t count, int(comp)(const data_type &, const data_type &))
+	{
+		size_t index_i = 1;
+		while (index_i < count)
+		{
+			size_t index_j = index_i;
+			while ((index_j > 0) && (comp(pntr[index_j - 1], pntr[index_j]) == 1))
+				std::swap(pntr[index_j - 1], pntr[index_j]);
+			index_i += 1;
+		}
+	}
 }
